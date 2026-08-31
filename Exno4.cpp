23be113755ec2a71ddc71cce95546a6567260883
aaditipawar8433 <aaditipawar8433@gmail.com>
2026-08-31 17:50:06 +0000

    Exno4

---
+++ Exno4.cpp
@@ -0,0 +1,45 @@
+ #include<iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    float length;
+    float width;
+
+public:
+    Rectangle(float len, float wid) {
+        length = len;
+        width = wid;
+    }
+
+    float dispLength() {
+        return length;
+    }
+
+    float dispWidth() {
+        return width;
+    }
+
+    ~Rectangle() {
+        cout << "Rectangle destroyed" << endl;
+    }
+
+    float Area() {
+        return length * width;
+    }
+
+    float Perimeter() {
+        return 2 * (length + width);
+    }
+};
+
+int main() {
+    Rectangle value(3, 4);
+
+    cout << "Length of rectangle: " << value.dispLength() << endl;
+    cout << "Width of rectangle: " << value.dispWidth() << endl;
+    cout << "Area of rectangle is: " << value.Area() << endl;
+    cout << "Perimeter of rectangle is: " << value.Perimeter() << endl;
+
+    return 0;
+}
